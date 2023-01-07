a6cb62a8680b7ac4dd91a6ea472366fa8539c6e1
Mattias Malk <matmal@ttu.ee>
2023-01-07 21:47:25 +0200

    WIP point markers

--- app/src/main/java/com/example/endotastic/MapsActivity.kt
+++ app/src/main/java/com/example/endotastic/MapsActivity.kt
@@ -545,10 +545,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     //endregion
 
-    /*
-
-     */
-
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
